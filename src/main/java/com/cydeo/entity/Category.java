2d2ffca905d613cca56e35262591cee8d2c0b8c3
MelikeAbliz <medine20171121@gmail.com>
2024-05-12 19:03:15 +0300

    create table relationship

---
+++ src/main/java/com/cydeo/entity/Category.java
@@ -0,0 +1,12 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Category extends BaseEntity {
+    private String name;
+}
