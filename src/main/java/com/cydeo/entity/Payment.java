9d75d9a4f8eb4d2f582bf83034dad9e7f14b49ad
MelikeAbliz <medine20171121@gmail.com>
2024-05-12 19:43:59 +0300

    cartItem,order,payment table created

--- src/main/java/com/cydeo/entity/Payment.java
+++ src/main/java/com/cydeo/entity/Payment.java
@@ -1,5 +1,26 @@
 package com.cydeo.entity;
 
-public class Payment {
+import com.cydeo.enums.PaymentMethod;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Payment extends BaseEntity {
+
+    @Column(precision = 19,scale = 2)//precision->total digits number ,like 12345678912345678.23
+    private BigDecimal paidPrice;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
 
 }
